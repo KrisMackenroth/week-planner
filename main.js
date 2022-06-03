337136588f3caa1b8144e42d93ec36bfc4319b79
Chorong Shin <chorongstellashin@gmail.com>
2022-06-03 18:00:08 +0000

    In process

---
+++ main.js
@@ -0,0 +1,17 @@
+var $addEntryButton = document.querySelector('.add-entry-button');
+var $modal = document.querySelector('.modal');
+// var $closeButton = document.querySelector('.close');
+var $overlay = document.querySelector('.overlay');
+
+$addEntryButton.addEventListener('click', function (event) {
+  event.target.className = 'open';
+  $overlay.className = 'overlay on';
+  $modal.className = 'show';
+});
+
+// $closeButton.addEventListener('click', function (event) {
+//   event.target.className = 'close';
+//   $overlay.className = 'overlay';
+//   $modal.className = 'modal hide';
+
+// });
