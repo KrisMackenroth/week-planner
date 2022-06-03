5b4542682a579c41a88ef37fcc4d0bcd295c843c
Collin <collinhovey@gmail.com>
2022-06-03 19:50:41 +0000

    Started javascript

--- main.js
+++ main.js
@@ -10,9 +10,12 @@
 
 var $addEntryButton = document.querySelector('.add-entry-button');
 var $modal = document.querySelector('.modal');
-var $closeButton = document.querySelector('.close');
+// var $closeButton = document.querySelector('.close');
 var $overlay = document.querySelector('.overlay');
 var $form = document.querySelector('form');
+// var $weekday = document.querySelector('select.days');
+// var $timeOfDay = document.querySelector('.time');
+// var $taskDescription = document.querySelector('#description');
 
 $addEntryButton.addEventListener('click', function (event) {
   event.target.className = 'open';
@@ -20,13 +23,20 @@ $addEntryButton.addEventListener('click', function (event) {
   $modal.className = 'show';
 });
 
-$closeButton.addEventListener('click', function (event) {
-  event.target.className = 'close';
-  $overlay.className = 'overlay';
-  $modal.className = 'modal hide';
-});
+// $closeButton.addEventListener('click', function (event) {
+//   event.target.className = 'close';
+//   $overlay.className = 'overlay';
+//   $modal.className = 'modal hide';
+// });
 
 $form.addEventListener('submit', function (event) {
   event.defaultPrevented();
 
 });
+
+// function addEntry(event) {
+//   var newTask = {};
+//   newTask.time = $timeOfDay;
+//   newTask.description = $taskDescription;
+//   data.$weekday.push(newTask);
+// }
