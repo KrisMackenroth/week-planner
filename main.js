eb8c467b35fea4420794f8121dbdcd33d2078a08
Collin <collinhovey@gmail.com>
2022-06-03 22:27:37 +0000

    Started tables

--- main.js
+++ main.js
@@ -19,12 +19,19 @@ var $addEntryButton = document.querySelector('.add-entry-button');
 var $modal = document.querySelector('.modal');
 var $closeButton = document.querySelector('.close');
 var $overlay = document.querySelector('.overlay');
-// var $form = document.querySelector('form');
+var $form = document.querySelector('form');
 // var $weekday = document.querySelector('select option:checked');
 // var $timeOfDay = document.querySelector('.time');
 // var $taskDescription = document.querySelector('#description');
 // var mockTable = document.querySelector('.mock-table');
 // var $options = document.querySelectorAll('option');
+// var $mondayButton = document.querySelector('.monday');
+// var $tuesdayButton = document.querySelector('.tuesday');
+// var $wednesdayButton = document.querySelector('.wednesday');
+// var $thursdayButton = document.querySelector('.thursday');
+// var $fridayButton = document.querySelector('.friday');
+// var $saturdayButton = document.querySelector('.saturday');
+// var $sundayButton = document.querySelector('.sunday');
 
 $addEntryButton.addEventListener('click', function (event) {
   event.target.className = 'open';
@@ -38,25 +45,24 @@ $closeButton.addEventListener('click', function (event) {
   $modal.className = 'modal hide';
 });
 
-// $form.addEventListener('submit', function (event) {
-// var entry = {
-//   day: $form.elements.day.value,
-//   time: $form.elements.time.value,
-//   description: $form.elements.description.value,
-//   entryId: data.nextEntryId
-// };
+$form.addEventListener('submit', function (event) {
+  // var entry = {
+  //   day: $form.elements.day.value,
+  //   time: $form.elements.time.value,
+  //   description: $form.elements.description.value,
+  //   entryId: data.nextEntryId
+  // };
 
-//   var emptyObject = {};
-//   emptyObject.day = $form.date.value;
-//   emptyObject.time = $form.time.value;
-//   emptyObject.description = $form.description.value;
-//   data.entries.unshift(emptyObject);
-//   // addEntry();
-//   mockTable.prepend(createTable(emptyObject));
-
-//   $form.reset();
-//   data.nextEntryId++;
-// });
+  var emptyObject = {};
+  emptyObject.day = $form.date.value;
+  emptyObject.time = $form.time.value;
+  emptyObject.description = $form.description.value;
+  data.entries.unshift(emptyObject);
+  // addEntry();
+  // mockTable.prepend(createTable(emptyObject));
+  $form.reset();
+  data.nextEntryId++;
+});
 
 // function addEntry(event) {
 //   var newTask = {};
