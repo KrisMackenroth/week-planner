32ad59af311e2d1c7dc64a749a22f02c8f3db653
Chorong Shin <chorongstellashin@gmail.com>
2022-06-03 19:04:08 +0000

    in process

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ var $addEntryButton = document.querySelector('.add-entry-button');
 var $modal = document.querySelector('.modal');
 var $closeButton = document.querySelector('.close');
 var $overlay = document.querySelector('.overlay');
+var $form = document.querySelector('form');
 
 $addEntryButton.addEventListener('click', function (event) {
   event.target.className = 'open';
@@ -23,4 +24,9 @@ $closeButton.addEventListener('click', function (event) {
   event.target.className = 'close';
   $overlay.className = 'overlay';
   $modal.className = 'modal hide';
+});
+
+$form.addEventListener('submit', function (event) {
+  event.defaultPrevented();
+
 });
